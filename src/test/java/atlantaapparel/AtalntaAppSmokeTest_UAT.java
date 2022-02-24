a12316ff946d167360312b5c7b210ec55c58ead8
nilamma <nilamma@cybage.com>
2022-02-24 17:27:31 +0530

    Added code for ATLApp_MPTcs, Exh Direct, Global Search

--- src/test/java/atlantaapparel/AtalntaAppSmokeTest_UAT.java
+++ src/test/java/atlantaapparel/AtalntaAppSmokeTest_UAT.java
@@ -85,9 +85,9 @@ public class AtalntaAppSmokeTest_UAT extends base {
 		amhe = new AMHeaderLinksPage(driver);
 		amab = new AMAboutTabPage(driver);
 
-		//Navigate to Atlanta Market site
+		//Navigate to Atlanta Apparel site
 		driver.manage().window().maximize();
-		driver.get(prop.getProperty("ammarturl"));;
+		driver.get(prop.getProperty("atlappurl"));;
 		Thread.sleep(8000);
 		lap.getIUnderstandBtn().click();
 		Thread.sleep(10000);
@@ -96,13 +96,11 @@ public class AtalntaAppSmokeTest_UAT extends base {
 	@Test(priority=1)
 	public void TS001_VerifyMarketPlannerLoginTest() throws InterruptedException, IOException
 	{
-
 		//The purpose of this test case to verify:-
-		//UXP-T101: To verify the Market Planner overview and it's functionality
+		//UXP-T01: To verify the Market Planner overview and it's functionality
 
 		lap = new UXPLandingPage(driver);
 		lp = new UXPLoginPage(driver);
-		amhe = new AMHeaderLinksPage(driver);
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
@@ -113,8 +111,109 @@ public class AtalntaAppSmokeTest_UAT extends base {
 		Assert.assertTrue(lap.getMPLinkText().isDisplayed());
 	}
 
+	@Test(priority=2)
+	public void TS002_VerifyMarketPlannerProfileOptionTest() throws InterruptedException, IOException
+	{
+		//The purpose of this test case to verify:-
+		//UXP-002: To verify Profile option in Market Planner
 
+		lap = new UXPLandingPage(driver);
+		atlps = new ATLProfileAndSettingsPage(driver);
 
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		//Click Profile and Settings tab under Market Planner and verify result
+		atlps.getATLWelcomeText().click();
+		atlps.getATLProfileAndSettings().click();
+		Thread.sleep(10000);
+		Assert.assertTrue(driver.getCurrentUrl().contains(prop.getProperty("atlappurl") + "Profile"));
+		System.out.println("Profile Page is displayed successfully");
+	}
+	
+	@Test(priority=3)
+	public void TS003_VerifyMarketPlannerExhibitorPortalOptionTest() throws InterruptedException, IOException
+	{
+		//The purpose of this test case to verify:-
+		//UXP-003: To verify Exhibitor Portal option in Market Planner
+
+		lap = new UXPLandingPage(driver);
+		atlps = new ATLProfileAndSettingsPage(driver);
+		amexh = new AMExhibitTabPage(driver);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		atlps.getATLWelcomeText().click();
+		atlps.getAMCExhibitorPortalOptn().click();
+		
+		//Verify that 'EXP Login' page should be displayed
+		Thread.sleep(5000);
+		Assert.assertTrue(amexh.getExpPortalHeader().getText().contains("Sign in with your existing account"));
+	
+		driver.get(prop.getProperty("atlappurl"));
+		
+		/*//Sign out from Market Planner
+		atlps.getATLWelcomeText().click();
+		lap.getMPSignOutBtn().click();
+		Thread.sleep(10000);*/
+	}
+	
+	@Test(priority=4)
+	public void TS004_VerifyGlobalSearchFunctionalityTest() throws InterruptedException, IOException
+	{
+		//The purpose of this test case to verify:-
+		//UXP-T04: To verify global search functionality
+
+		lap = new UXPLandingPage(driver);
+		lp = new UXPLoginPage(driver);
+		gs = new UXPGlobalSearchPage(driver);
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		gs.getGlobalSearchTextBox().sendKeys((prop.getProperty("globalsearchinput")));
+		gs.getSearchButton().click();
+		String searchterm = gs.getVerifyGlobalSeacrh().getText();
+		Assert.assertTrue(searchterm.contains(prop.getProperty("globalsearchinput")));
+		System.out.println("Global Search functionality is working properly.");
+	}
+	
+	@Test(priority=5)
+	public void TS005_VerifyExhibitorDirectoryTest() throws InterruptedException, IOException
+	{
+		//The purpose of this test case to verify:-
+		//UXP-T05: To verify Exhibitor Directory
+
+		lap = new UXPLandingPage(driver);
+		lp = new UXPLoginPage(driver);
+		amhe = new AMHeaderLinksPage(driver);
+		ed = new UXPExhibitorDirectoryPage(driver);
+		gs = new UXPGlobalSearchPage(driver);
+		exhp = new UXPExhibitorsAndProductsTabPage(driver);
+		mi = new UXPMarketInfoPage(driver);
+		atled = new ATLExhibitorDirectoryPage (driver);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		//Verify exhibitor directory page is successfully opened
+		atled.getATLExhibitorDirectory().click();
+		Assert.assertEquals(exhp.getVerifyExhibitorDirectory().getText(), "Atlanta Apparel Exhibitor Directory");
+
+		//Scroll till exhibitor directory
+		utl.scrollToElement(exhp.getVerifyExhibitorDirectory());
+
+		//Verify exhibitor data is displayed or not as per search criteria
+		System.out.println("Exhibitor Directory is opened properly.");
+		gs.getGlobalSearchTextBox().sendKeys((prop.getProperty("exhibitordirectory")));
+		gs.getSearchButton().click();
+
+		utl.scrollToElement(mi.getVerifyContactUs());
+		amhe.getExhbAndProdsTab().click();
+		gs.getSearchButton().click();
+		utl.scrollToElement(mi.getVerifyContactUs());
+		String searchterm = gs.getVerifyGlobalSeacrh().getText();
+		Assert.assertTrue(searchterm.contains(prop.getProperty("exhibitordirectory")));
+		System.out.println("Exhibitor Directory page is working properly.");
+		//driver.get(prop.getProperty("atlappurl"));
+	}
 	/*@AfterSuite
 	public void sendEmail()
 	{
