e6abcf8f52b67be275aabcc508ac3246976588d5
Nilam Mahajan <nilamma@cybage.com>
2022-05-16 09:11:41 +0000

    Updated AtlantaMarketSmokeTest_PROD.java

--- src/test/java/PROD/AtlantaMarketSmokeTest_PROD.java
+++ src/test/java/PROD/AtlantaMarketSmokeTest_PROD.java
@@ -91,8 +91,8 @@ public class AtlantaMarketSmokeTest_PROD extends base {
 		driver.manage().window().maximize();
 		driver.get(prop.getProperty("atlmrkturl"));
 		Thread.sleep(5000);
-		//		lap.getIUnderstandBtn().click();
-		//		Thread.sleep(3000);
+		lap.getIUnderstandBtn().click();
+		Thread.sleep(3000);
 
 		//Login to Market Planner
 		utl.verifyMPLoginFunctionality();
