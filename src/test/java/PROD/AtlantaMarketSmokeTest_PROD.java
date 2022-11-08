b6e4accbfee51e8ff9a72c76b157d001c45568fd
Nilam Mahajan <nilamma@cybage.com>
2022-11-08 10:26:14 +0000

    Updated AtlantaMarketSmokeTest_PROD.java

--- src/test/java/PROD/AtlantaMarketSmokeTest_PROD.java
+++ src/test/java/PROD/AtlantaMarketSmokeTest_PROD.java
@@ -67,6 +67,7 @@ public class AtlantaMarketSmokeTest_PROD extends base {
 	public void initialize() throws IOException, InterruptedException
 	{
 		driver = initializeDriver(); //requires for Parallel text execution
+		DOMConfigurator.configure("log4j.xml");
 				utl = new Utility(driver);
 			lap = new UXPLandingPage(driver);
 
