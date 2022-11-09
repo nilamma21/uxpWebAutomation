03682b451886b05187818928eb34286037fffd4a
Nilam Mahajan <nilamma@cybage.com>
2022-11-09 07:06:59 +0000

    Updated ExhibitorPortalSmokeTest_PROD.java

--- src/test/java/PROD/ExhibitorPortalSmokeTest_PROD.java
+++ src/test/java/PROD/ExhibitorPortalSmokeTest_PROD.java
@@ -44,8 +44,8 @@ public class ExhibitorPortalSmokeTest_PROD extends base {
 	@BeforeClass
 	public void initialize() throws IOException, InterruptedException
 	{
-		driver = initializeDriver(); //requires for Parallel text execution
 		DOMConfigurator.configure("log4j.xml");
+		driver = initializeDriver(); //requires for Parallel text execution
 //		utl = new Utility(driver);
 //		lap = new UXPLandingPage(driver);
 
