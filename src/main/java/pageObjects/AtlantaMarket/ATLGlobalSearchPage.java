56a374dca43e5938d72fff776cf2c6ef3f366545
Nilam Mahajan <nilamma@cybage.com>
2022-12-09 05:20:55 +0000

    Updated ATLGlobalSearchPage.java

--- src/main/java/pageObjects/AtlantaMarket/ATLGlobalSearchPage.java
+++ src/main/java/pageObjects/AtlantaMarket/ATLGlobalSearchPage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import bsh.This;
+//import bsh.This;
 
 public class ATLGlobalSearchPage {
 
