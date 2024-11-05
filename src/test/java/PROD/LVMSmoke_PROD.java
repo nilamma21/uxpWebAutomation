143aa3a862fba808c95b2cf8ccb6728467a39668
SwapnilIrnak91 <swapnili@cybage.com>
2024-11-05 10:21:09 +0530

    uxpSmokecode Added

--- src/test/java/PROD/LVMSmoke_PROD.java
+++ src/test/java/PROD/LVMSmoke_PROD.java
@@ -2,21 +2,13 @@ package PROD;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import io.reactivex.rxjava3.functions.Action;
 import pageObjects.LasVegasMarket.UXPAttendPage;
 import pageObjects.LasVegasMarket.UXPExhibitPage;
 import pageObjects.LasVegasMarket.UXPExhibitorDirectoryPage;
